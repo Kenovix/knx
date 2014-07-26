f9dd475bf09862b7062b41f961bebf6290ed27f0
Hernando Bocanegra <hbocanegra@kenovix.com>
2014-07-25 23:29:31 -0500

    Se ajustan cambios en el menu de farmacia del admin

--- src/knx/FacturacionBundle/Menu/Builder.php
+++ src/knx/FacturacionBundle/Menu/Builder.php
@@ -92,54 +92,47 @@ class Builder extends ContainerAware
 			
                 }elseif($security->isGranted('ROLE_ADMIN')){
                     
-                    
-                     $menu->addChild('farmacia', array('uri' => '#'));
-			$menu['farmacia']->addChild('Nueva', array('uri' => '#'));
-			$menu['farmacia']->addChild('Ingresos', array('route' => 'ingreso_list'));
-			$menu['farmacia']->addChild('Movimientos', array('uri' => '#'));
-			$menu['farmacia']->addChild('Pyp', array('route' => 'imvpyp_search'));
-                        $menu['farmacia']['Movimientos']->addChild('Traslados', array('uri' => '#'));
-			$menu['farmacia']['Movimientos']['Traslados']->addChild('Listar/Nuevo', array('route' => 'traslado_list', 'routeParameters' => array('char' => 'A')));
-			$menu['farmacia']['Movimientos']['Traslados']->addChild('Imprimir', array('route' => 'traslado_searchprint'));
-			$menu['farmacia']['Movimientos']->addChild('Devoluciones Proveedor', array('uri' => '#'));
-			$menu['farmacia']['Movimientos']['Devoluciones Proveedor']->addChild('Listar/Nuevo', array('route' => 'devolucion_list', 'routeParameters' => array('char' => 'A')));
-			$menu['farmacia']['Movimientos']['Devoluciones Proveedor']->addChild('Imprimir', array('route' => 'devolucion_searchprint'));
-			$menu['farmacia']['Nueva']->addChild('Farmacia', array('route' => 'farmacia_list', 'routeParameters' => array('char' => 'A')));
-			$menu['farmacia']['Nueva']->addChild('Categoria', array('route' => 'categoria_list', 'routeParameters' => array('char' => 'A')));
-			$menu['farmacia']['Nueva']->addChild('Existencia', array('uri' => '#'));
-			$menu['farmacia']['Nueva']['Existencia']->addChild('Listar/Nueva', array('route' => 'imv_search'));
-			$menu['farmacia']['Nueva']['Existencia']->addChild('Imprimir', array('route' => 'imv_searchimprimir'));
-			$menu['farmacia']->addChild('Almacen', array('uri' => '#'));
-			$menu['farmacia']['Almacen']->addChild('Consultar', array('route' => 'almacenimv_search'));
-			$menu['farmacia']['Almacen']->addChild('Imprimir', array('route' => 'almacenimv_searcha'));
-			
-           	$menu->addChild('Facturación', array('uri' => '#'));
-			$menu['Facturación']->addChild('Facturar', array('uri' => '#'));
-			$menu['Facturación']['Facturar']->addChild('Consulta', array('route' => 'facturacion_consulta_new'));
-			$menu['Facturación']['Facturar']->addChild('Procedimiento', array('uri' => '#'));
-			$menu['Facturación']['Facturar']['Procedimiento']->addChild('Ambulatorio', array('route' => 'facturacion_procedimiento_new', 'routeParameters' => array('tipo' => 'A')));
-			$menu['Facturación']['Facturar']['Procedimiento']->addChild('Urgencias', array('route' => 'facturacion_urgencias_list'));
-			$menu['Facturación']['Facturar']->addChild('Medicamento', array('route' => 'facturacion_insumo_new'));
-			$menu['Facturación']['Facturar']->addChild('Reportes', array('route' => 'reporte_cargo_new'));
-			$menu['Facturación']['Facturar']->addChild('Morbilidad', array('route' => 'morbilidad_vista'));
-                        
-			$menu->addChild('Usuarios', array('uri' => '#'));
-			$menu['Usuarios']->addChild('Listar', array('route' => 'usuario_list'));
-			$menu['Usuarios']->addChild('Crear', array('route' => 'fos_user_registration_register'));
-		
-			$menu->addChild($usuario->getUsername(), array('uri' => '#'));
-			$menu[$usuario->getUsername()]->addChild('Salir', array('route' => 'logout'));
-                    
-                    
-                    
-                    
-                    
-                    
-                }
-               
-	
+               		$menu->addChild('farmacia', array('uri' => '#'));
+					$menu['farmacia']->addChild('Nueva', array('uri' => '#'));
+					$menu['farmacia']->addChild('Ingresos', array('route' => 'ingreso_list'));
+					$menu['farmacia']->addChild('Movimientos', array('uri' => '#'));
+					$menu['farmacia']->addChild('Pyp', array('route' => 'imvpyp_search'));
+			        $menu['farmacia']['Movimientos']->addChild('Traslados', array('uri' => '#'));
+					$menu['farmacia']['Movimientos']['Traslados']->addChild('Listar/Nuevo', array('route' => 'traslado_list', 'routeParameters' => array('char' => 'A')));
+					$menu['farmacia']['Movimientos']['Traslados']->addChild('Imprimir', array('route' => 'traslado_searchprint'));
+					$menu['farmacia']['Movimientos']->addChild('Devoluciones Proveedor', array('uri' => '#'));
+					$menu['farmacia']['Movimientos']['Devoluciones Proveedor']->addChild('Listar/Nuevo', array('route' => 'devolucion_list', 'routeParameters' => array('char' => 'A')));
+					$menu['farmacia']['Movimientos']['Devoluciones Proveedor']->addChild('Imprimir', array('route' => 'devolucion_searchprint'));
+					$menu['farmacia']['Nueva']->addChild('Farmacia', array('route' => 'farmacia_list', 'routeParameters' => array('char' => 'A')));
+					$menu['farmacia']['Nueva']->addChild('Categoria', array('route' => 'categoria_list', 'routeParameters' => array('char' => 'A')));
+					$menu['farmacia']['Nueva']->addChild('Existencia', array('uri' => '#'));
+					$menu['farmacia']['Nueva']['Existencia']->addChild('Listar/Nueva', array('route' => 'imv_search'));
+					$menu['farmacia']['Nueva']['Existencia']->addChild('Imprimir', array('route' => 'imv_searchimprimir'));
+
+					$menu['farmacia']->addChild('Almacen', array('uri' => '#'));
+					$menu['farmacia']['Almacen']->addChild('Consultar', array('route' => 'almacenimv_search'));
+					$menu['farmacia']['Almacen']->addChild('Imprimir', array('route' => 'almacenimv_searcha'));
+
+			        $menu->addChild('Facturación', array('uri' => '#'));
+					$menu['Facturación']->addChild('Facturar', array('uri' => '#'));
+					$menu['Facturación']['Facturar']->addChild('Consulta', array('route' => 'facturacion_consulta_new'));
+					$menu['Facturación']['Facturar']->addChild('Procedimiento', array('uri' => '#'));
+					$menu['Facturación']['Facturar']['Procedimiento']->addChild('Ambulatorio', array('route' => 'facturacion_procedimiento_new', 'routeParameters' => array('tipo' => 'A')));
+					$menu['Facturación']['Facturar']['Procedimiento']->addChild('Urgencias', array('route' => 'facturacion_urgencias_list'));
+					$menu['Facturación']['Facturar']->addChild('Medicamento', array('route' => 'facturacion_insumo_new', 'routeParameters' => array('tipo' => 'A')));
+					$menu['Facturación']['Facturar']->addChild('Reportes', array('route' => 'reporte_cargo_new'));
+					$menu['Facturación']['Facturar']->addChild('Morbilidad', array('route' => 'morbilidad_vista'));
+
+					$menu->addChild('Usuarios', array('uri' => '#'));
+					$menu['Usuarios']->addChild('Listar', array('route' => 'usuario_list'));
+					$menu['Usuarios']->addChild('Crear', array('route' => 'fos_user_registration_register'));
+
+					$menu->addChild($usuario->getUsername(), array('uri' => '#'));
+
+					$menu[$usuario->getUsername()]->addChild('Salir', array('route' => 'logout'));
+
+			}
+
 		return $menu;
 	}
-        
-                
 }
